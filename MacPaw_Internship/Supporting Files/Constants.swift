23c69f61ce6ae106c2a346bf4c5a2f8ff9efe6a2
Artem <artem@artems-mac.local>
2018-05-02 13:26:03 -0700

    Видалив не потрібні enums

--- MacPaw_Internship/Supporting Files/Constants.swift
+++ MacPaw_Internship/Supporting Files/Constants.swift
@@ -10,18 +10,4 @@ class Constants {
     public static let genres = ["RPG", "Action", "Strategy", "Simulator"]
     public static let filterCategories = ["Title", "Year", "Description", "Genre"]
     public static let cellIdentifier = "gameCell"
-    
-    public enum Genres {
-        case RPG
-        case Action
-        case Strategy
-        case Simulator
-    }
-    
-    public enum FilterCategories {
-        case Title
-        case Year
-        case Description
-        case Genre
-    }
 }
